0fe68ad16d294a50a8fb9a0233c55be6e8646980
Khavan <krajput123@hotmail.com>
2025-04-04 13:19:58 +0100

    finished main functionality of game

--- Rock-Paper-Scissors/game.js
+++ Rock-Paper-Scissors/game.js
@@ -1,22 +1,34 @@
 const prompt = require('prompt-sync')();
 
-function playerMove(p1, p2){
-    p1 = prompt('Enter Player 1 move: ')
-    p2 = prompt("Enter p2 move: ")
+function playerMove(p1, p2, rounds){
+    rounds = prompt("How many round would you like to play?")
+    let playerOneScore = 0
+    let playerTwoScore = 0
 
-    let playerOneCounter = 0
-    let playerTwoCounter = 0
-    let result = ''
+    while(rounds > 0){
+        p1 = prompt('Enter Player 1 move: ')
+        p2 = prompt("Enter player 2 move: ")
 
-    if(p1 == p2){
-        console.log('Draw!')
-    } else if ((p1 == 'rock' && p2 == 'scissors')
-        ||(p1 == 'scissors' && p2 == 'paper')
-        ||(p1 == 'paper' && p2 == 'rock')){
-        console.log('player 1 wins!')
-    } else {
-        console.log('player 2 wins!')
+        if(p1 == p2){
+            console.log('Draw!')
+        } else if ((p1 == 'rock' && p2 == 'scissors')
+            ||(p1 == 'scissors' && p2 == 'paper')
+            ||(p1 == 'paper' && p2 == 'rock')){
+            console.log('player 1 wins!')
+            playerOneScore++
+        } else {
+            console.log('player 2 wins!')
+            playerTwoScore++
     }
+        
+        rounds--
+    }
+
+    console.log('-------FINAL SCORE!------------')
+    console.log(`Player 1 Final Score: ${playerOneScore}`)
+    console.log(`Player 2 Final Score: ${playerTwoScore}`)
+    console.log('--------------------------------')
+    
 }
 
 playerMove()
