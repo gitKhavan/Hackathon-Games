35923370a8335f89b8fdad19fa798ddb0d9fdfa5
Khavan <krajput123@hotmail.com>
2025-04-04 11:39:59 +0100

    added player functionality

--- Rock-Paper-Scissors/game.js
+++ Rock-Paper-Scissors/game.js
@@ -1,10 +1,23 @@
+const prompt = require('prompt-sync')();
+
 function playerMove(p1, p2){
-    let p1 = 'rock'
-    let p2 = 'rock'
+    p1 = prompt('Enter Player 1 move: ')
+    p2 = prompt("Enter p2 move: ")
+
     let playerOneCounter = 0
     let playerTwoCounter = 0
-    
-    const game = ['rock', 'paper', 'scissors']
-    if(p1 === p2){ 
-    } 
+    let result = ''
+
+    if(p1 == p2){
+        console.log('Draw!')
+    } else if ((p1 == 'rock' && p2 == 'scissors')
+        ||(p1 == 'scissors' && p2 == 'paper')
+        ||(p1 == 'paper' && p2 == 'rock')){
+        console.log('player 1 wins!')
+    } else {
+        console.log('player 2 wins!')
+    }
 }
+
+playerMove()
+
