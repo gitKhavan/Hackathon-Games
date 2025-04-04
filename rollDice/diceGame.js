ac04c1df2615666628b0f448299418c5315dd085
Khavan <krajput123@hotmail.com>
2025-04-04 14:09:14 +0100

    completed roll dice game

---
+++ rollDice/diceGame.js
@@ -0,0 +1,20 @@
+const prompt = require('prompt-sync')();
+
+function rollDice(){
+
+    do{
+        answer = prompt("Would you like to roll the dice? y/n ")
+    } while(answer !== 'y' && answer !== 'n')
+
+    
+    const rndInt = Math.floor(Math.random() * 6) + 1
+
+    if(answer == 'y'){
+        console.log(`You rolled: ${rndInt}`);
+    } else {
+        console.log('Bye!');
+    }
+    
+}
+
+rollDice()
