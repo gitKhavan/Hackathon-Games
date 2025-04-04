3b277802992d299818545bd465ca217c7c4135c7
Khavan <krajput123@hotmail.com>
2025-04-04 11:07:24 +0100

    added function playerMove

---
+++ Rock-Paper-Scissors/game.js
@@ -0,0 +1,10 @@
+function playerMove(p1, p2){
+    let p1 = 'rock'
+    let p2 = 'rock'
+    let playerOneCounter = 0
+    let playerTwoCounter = 0
+    
+    const game = ['rock', 'paper', 'scissors']
+    if(p1 === p2){ 
+    } 
+}
